9f3568b042906d78e21ee9d8982f55117ca6c4a7
victorgusto <victorgusto@gmail.com>
2022-07-31 17:12:27 -0300

    Inclusão do Firebaes e ajustes gerais
    Foi feita a inclusão do app no firebase e também feito alguns ajustes e melhorias.

--- app/src/main/java/me/dio/simulator/ui/MainActivity.java
+++ app/src/main/java/me/dio/simulator/ui/MainActivity.java
@@ -5,12 +5,14 @@ import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -29,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     private ActivityMainBinding binding;
-    private MatchesApi mathcesApi;
-    private MatchesAdapter matchesAdapter;
+    private MatchesApi matchesApi;
+    private MatchesAdapter matchesAdapter = new MatchesAdapter(Collections.emptyList());
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,12 +52,13 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl("https://victorgusto.github.io/matches-simulator-api/")
                 .addConverterFactory(GsonConverterFactory.create()).build();
 
-        mathcesApi = retrofit.create(MatchesApi.class);
+        matchesApi = retrofit.create(MatchesApi.class);
     }
 
     private void setupMatchesList() {
         binding.rvMatches.setHasFixedSize(true);
         binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvMatches.setAdapter(matchesAdapter);
         findMatchesFromApi();
     }
 
@@ -66,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
     private void setupFloatingActionButton() {
         binding.fabSimulator.setOnClickListener(
                 view -> {
+
                     view.animate().rotationBy(360).setDuration(500).setListener(new AnimatorListenerAdapter() {
                         @Override
                         public void onAnimationEnd(Animator animation) {
@@ -87,11 +91,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void findMatchesFromApi() {
         binding.srlMatches.setRefreshing(true);
-        mathcesApi.getMatches().enqueue(new Callback<List<Match>>() {
+        matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
                                             @Override
-                                            public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+                                            public void onResponse(@NonNull Call<List<Match>> call, @NonNull Response<List<Match>> response) {
                                                 if (response.isSuccessful()) {
                                                     List<Match> matches = response.body();
+                                                    assert matches != null;
                                                     Log.i("SIMULATOR", "Deu tudo certo! Voltaram partidas: " + matches.size());
                                                     matchesAdapter = new MatchesAdapter(matches);
                                                     binding.rvMatches.setAdapter(matchesAdapter);
@@ -103,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                                             }
 
                                             @Override
-                                            public void onFailure(Call<List<Match>> call, Throwable t) {
+                                            public void onFailure(@NonNull Call<List<Match>> call, @NonNull Throwable t) {
                                                 showErrorMessage();
                                                 binding.srlMatches.setRefreshing(false);
                                             }
